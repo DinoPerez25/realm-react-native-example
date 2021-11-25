4a4942c3486ea7d6e5047f825da45657b111cebc
DinoPerez25 <dperezhincapie@gmail.com>
2021-11-25 10:37:26 -0500

    Add error caption to select component

--- Components/ControlledSelect.js
+++ Components/ControlledSelect.js
@@ -1,38 +1,58 @@
-import React from 'react'; import
-RNPickerSelect from 'react-native-picker-select';
+import React, { Fragment } from 'react';
+import { Text, StyleSheet } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 import { Controller } from 'react-hook-form';
 
 const ControlledSelect = ({
   control,
+  error,
   name,
   label,
   selectOptions,
   selectProps,
   controllerProps,
+  textInputProps,
 }) => {
   return (
-    <Controller
-      {...controllerProps}
-      control={control}
-      name={name}
-      render={({ field: { onChange, onBlur, value } }) => (
-        <RNPickerSelect
-          {...selectProps}
-          onBlur={(event) => {
-            onBlur(event);
-            if (selectProps.onBlur) {
-              selectProps.onBlur(event);
-            }
-          }}
-          label={label}
-          onSelect={(index) => onChange(index)}
-          onValueChange={(value) => onChange(value)}
-          items={selectOptions}
-          value={value}
-        />
-      )}
-    />
+    <Fragment>
+      <Controller
+        {...controllerProps}
+        control={control}
+        name={name}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <RNPickerSelect
+            {...selectProps}
+            style={styles.inputStyle}
+            textInputProps={textInputProps}
+            onBlur={(event) => {
+              onBlur(event);
+              if (selectProps.onBlur) {
+                selectProps.onBlur(event);
+              }
+            }}
+            label={label}
+            onSelect={(index) => onChange(index)}
+            onValueChange={(value) => onChange(value)}
+            items={selectOptions}
+            value={value}
+          />
+        )}
+      />
+      {error && <Text style={styles.error}>{error.message}</Text>}
+    </Fragment>
   )
 };
 
 export default ControlledSelect;
+const styles = StyleSheet.create({
+  error: {
+    color: 'red'
+  },
+  inputStyle: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 4,
+    paddingLeft: 15,
+    width: '100%',
+  },
+});
